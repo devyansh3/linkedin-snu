ceb7b82a3113ebe636ee629a8c10460674a09c77
Devyansh Sehgal <65131029+devyansh3@users.noreply.github.com>
2022-07-03 08:01:08 +0530

    now working sign in

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAycOQtUu3oIAn_L80xxHAoUgnGayXU2-0",
+  authDomain: "linkedin-snu.firebaseapp.com",
+  projectId: "linkedin-snu",
+  storageBucket: "linkedin-snu.appspot.com",
+  messagingSenderId: "502139238512",
+  appId: "1:502139238512:web:a63c2e48b5136e2223dcb5"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
